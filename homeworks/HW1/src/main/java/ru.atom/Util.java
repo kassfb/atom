29c5b7a9d295ae0a3daf1f6180a8fc857155afc2
kassfb <https://github.com/kassfb>
2020-09-28 09:32:03 +0300

    7\8 tests complete

--- homeworks/HW1/src/main/java/ru.atom/Util.java
+++ homeworks/HW1/src/main/java/ru.atom/Util.java
@@ -25,7 +25,13 @@ public class Util {
      * @return the largest of values.
      */
     public static int max(int[] values) {
-        throw new UnsupportedOperationException();
+        int lenght=values.length;
+        int max=values[0];
+        for (int i=0; i<lenght; i++){
+            if(max<values[i]) max=values[i];
+        }
+        return max;
+        //throw new UnsupportedOperationException();
     }
 
     /**
@@ -35,7 +41,13 @@ public class Util {
      * @return the sum of all values.
      */
     public static long sum(int[] values) {
-        throw new UnsupportedOperationException();
+        int lenght=values.length;
+        int sum=0;
+        for (int i=0; i<lenght; i++){
+            sum+=values[i];
+        }
+        return sum;
+        //throw new UnsupportedOperationException();
     }
 
 
