2411ddce972ae57a935a9320f5b5bae7025dbb0f
kassfb <https://github.com/kassfb>
2020-10-09 16:03:06 +0400

    8/8 tests passed

--- homeworks/HW1/src/main/java/ru.atom/Util.java
+++ homeworks/HW1/src/main/java/ru.atom/Util.java
@@ -10,7 +10,7 @@ public class Util {
     public static void main(String[] args) {
         int[] myArray = {1, 2, 3};
         System.out.println("max: " + max(myArray));
-        System.out.println("sum: " + max(myArray));
+        System.out.println("sum: " + sum(myArray));
         System.out.println(getHelloWorld());
     }
 
